08ba4c925b4348e7533294e86a96c48078ee1e21
antocecere <antocecere77@gmail.com>
2019-12-13 07:30:54 +0100

    Added animation callback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  keyframes,
+  group
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,26 +15,27 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
   animations: [
     trigger('divState', [
       state('normal', style({
-        'background-color':'red',
+        'background-color': 'red',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        'background-color':'blue',
+        'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
-      transition('normal <=> highlighted', animate(300))
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
     ]),
     trigger('wildState', [
       state('normal', style({
-        'background-color':'red',
+        'background-color': 'red',
         transform: 'translateX(0) scale(1)'
       })),
       state('highlighted', style({
-        'background-color':'blue',
-        transform: 'translateX(100px), scale(1)'
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
       })),
       state('shrunken', style({
-        'background-color':'green',
+        'background-color': 'green',
         transform: 'translateX(0) scale(0.5)'
       })),
       transition('normal => highlighted', animate(300)),
@@ -57,7 +66,7 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
         animate(300, style({
           transform: 'translateX(100px)',
           opacity: 0
-        }))        
+        }))
       ])
     ]),
     trigger('list2', [
@@ -68,7 +77,7 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
       transition('void => *', [
         animate(1000, keyframes([
           style({
-            transform: 'translateX(-100px)', 
+            transform: 'translateX(-100px)',
             opacity: 0,
             offset: 0
           }),
@@ -89,17 +98,18 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
           })
         ]))
       ]),
-      transition('* => void', [   
+      transition('* => void', [
         group([
           animate(300, style({
-          color: 'red'
-        })),
+            color: 'red'
+          })),
           animate(800, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))]),                    
+            transform: 'translateX(100px)',
+            opacity: 0
+          }))
+        ])
       ])
-    ])
+    ]),
   ]
 })
 export class AppComponent {
@@ -107,20 +117,28 @@ export class AppComponent {
   wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-    onAnimate() {
-      this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-      this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
-    }
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
 
-    onShrink() {      
-      this.wildState = 'shrunken';
-    }
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
-    onAdd(item) {
-      this.list.push(item);
-    }
+  onAdd(item) {
+    this.list.push(item);
+  }
 
-    onDelete(item) {
-      this.list.splice(this.list.indexOf(item), 1);
-    }
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+
+  animationStarted(event) {
+    console.log(event);
+  }
+
+  animationEnded(event) {
+    console.log(event);
+  }
 }
